6de05cb547e00065c32a7aa994d9bd622c06a1e8
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2019-03-25 14:52:30 +0100

    build: add header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Game extends React.Component {
+  render() {
+    return <TitleGame />;
+  }
+}
+
+class TitleGame extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Jeu du pendu</h1>
+      </header>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById('root'));
