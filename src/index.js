aa862ec389993c6de25ff0d829509cfffcb96ade
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2019-03-25 15:11:19 +0100

    build: add keyboard class and css style

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ class Game extends React.Component {
         <section>
           <Canva />
           <MysteryWord />
+          <Keyboard />
         </section>
       </div>
     );
@@ -31,6 +32,46 @@ class Canva extends React.Component {
   }
 }
 
+class Keyboard extends React.Component {
+  render() {
+    return (
+      <div id="keyboard-box">
+        <div class="keyboard-row">
+          <div class="key">A</div>
+          <div class="key">B</div>
+          <div class="key">C</div>
+          <div class="key">D</div>
+          <div class="key">E</div>
+          <div class="key">F</div>
+          <div class="key">G</div>
+          <div class="key">H</div>
+          <div class="key">I</div>
+          <div class="key">J</div>
+          <div class="key">K</div>
+        </div>
+        <div class="keyboard-row">
+          <div class="key">L</div>
+          <div class="key">M</div>
+          <div class="key">N</div>
+          <div class="key">O</div>
+          <div class="key">P</div>
+          <div class="key">Q</div>
+          <div class="key">R</div>
+          <div class="key">S</div>
+          <div class="key">T</div>
+        </div>
+        <div class="keyboard-row">
+          <div class="key">U</div>
+          <div class="key">V</div>
+          <div class="key">W</div>
+          <div class="key">X</div>
+          <div class="key">Y</div>
+          <div class="key">Z</div>
+        </div>
+      </div>
+    );
+  }
+}
 class MysteryWord extends React.Component {
   render() {
     return (
