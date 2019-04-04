75db3062e5931bf98d8af49ae569485db6bf698c
Wilfried Cottineau <wilfriedcottineau@gmail.com>
2019-04-04 19:36:25 +0200

    feat: change The KeyBoard to be a function component

--- src/index.js
+++ src/index.js
@@ -61,38 +61,36 @@ class Canva extends React.Component {
   }
 }
 
-function KeyboardKey(props) {
+function KeyboardKey({ value, onClick }) {
   return (
-    <div className="key" onClick={props.onClick}>
-      {props.value}
+    <div className="key" onClick={onClick}>
+      {value}
     </div>
   );
 }
 
-class Keyboard extends React.Component {
-  renderKeyboardKey(arrayOfLetters) {
+function Keyboard({ onClick }) {
+  const keysRow1 = Array.of('A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P');
+  const keysRow2 = Array.of('Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M');
+  const keysRow3 = Array.of('W', 'X', 'C', 'V', 'B', 'N');
+
+  function renderKeyboardKey(arrayOfLetters) {
     return arrayOfLetters.map((letter, index) => (
       <KeyboardKey
         value={letter}
         key={index.toString()}
-        onClick={() => this.props.onClick(letter)}
+        onClick={() => onClick(letter)}
       />
     ));
   }
 
-  render() {
-    const keysRow1 = Array.of('A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P');
-    const keysRow2 = Array.of('Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M');
-    const keysRow3 = Array.of('W', 'X', 'C', 'V', 'B', 'N');
-
-    return (
-      <div id="keyboard-box">
-        <div className="keyboard-row">{this.renderKeyboardKey(keysRow1)}</div>
-        <div className="keyboard-row">{this.renderKeyboardKey(keysRow2)}</div>
-        <div className="keyboard-row">{this.renderKeyboardKey(keysRow3)}</div>
-      </div>
-    );
-  }
+  return (
+    <div id="keyboard-box">
+      <div className="keyboard-row">{renderKeyboardKey(keysRow1)}</div>
+      <div className="keyboard-row">{renderKeyboardKey(keysRow2)}</div>
+      <div className="keyboard-row">{renderKeyboardKey(keysRow3)}</div>
+    </div>
+  );
 }
 
 const FRUITS = ['POMME', 'BANANE', 'ANANAS'];
